70b1a5cab5a162909461ba401ee5adcac3465df2
root <root@elara>
2019-11-17 18:11:11 +0100

    default route in main needed for olsrd

--- sbin/tdhook.sh
+++ sbin/tdhook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == "session.up" ]; then
     ifconfig $2 hw ether $MACADDR
-    dhcpcd -4GL $2
+    dhcpcd -4L $2
     ip route add default table uplink dev uplink
 else
     echo "Uncatched Hook: $*"
